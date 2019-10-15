f86c1ad9f78f2cf6a5fc77685696ca874e00c929
Gustavo Silva <gustavo_almeida11@hotmail.com>
2019-10-14 21:11:42 -0300

    trigger de auditoria

--- trigers.sql
+++ trigers.sql
@@ -1,4 +1,6 @@
-CREATE OR REPLACE
+
+-- Trigger para adicionar um sufixo ao inserir um novo departamento.
+ CREATE OR REPLACE
 TRIGGER TG_SUFIXO BEFORE
 INSERT
 	ON
@@ -6,9 +8,8 @@ INSERT
 BEGIN
 	:NEW.NOMEDEPTO := :NEW.NOMEDEPTO || 'sons';
 END;
-
-
-CREATE OR REPLACE
+-- Triger para não permitir que haja um professor com a titulação de doutor.
+ CREATE OR REPLACE
 TRIGGER TG_DOUTOR BEFORE
 INSERT
 	OR
@@ -19,4 +20,29 @@ BEGIN
 	IF :NEW.codtit = 1 THEN RAISE_APPLICATION_ERROR(-20000,
 	'Titulação Inválida');
 END IF;
+END;
+-- Trigger de auditoria, pega o horario e o usuário logado após cada alteração.
+ CREATE OR REPLACE
+TRIGGER tg_historico AFTER
+UPDATE
+	ON
+	professor FOR EACH ROW
+DECLARE DATA_ATUALIZACAO TIMESTAMP;
+
+LOGIN_ATUAL VARCHAR2(20);
+BEGIN
+DATA_ATUALIZACAO := CURRENT_TIMESTAMP;
+
+LOGIN_ATUAL := SYS_CONTEXT ('USERENV',
+'SESSION_USER');
+
+INSERT
+	INTO
+	PROFESSOR_HIST
+VALUES (:new.CODPROF,
+:new.CODDEPTO,
+:new.CODTIT,
+:new.NOMEPROF,
+DATA_ATUALIZACAO,
+LOGIN_ATUAL);
 END;
